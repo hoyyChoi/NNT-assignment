0a08a79f7fdd84c6c44596fdac195dbb4e720e00
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-02 00:06:21 +0900

    feat : listPage rendering

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,13 +1,35 @@
+import { useEffect, useState } from "react";
 import Category from "../components/ListPage/Category";
 import { ListTitle } from "../components/ListPage/ListTitle";
 import { WineList } from "../components/ListPage/WineList";
+import { getWineList } from "../apis/listPage";
+import { useSetRecoilState } from "recoil";
+import { titleState, wineListState } from "../recoil";
+import { Pagination } from "../components/ListPage/Pagination";
 
 export const ListPage = () => {
+	const [page, setPage] = useState<number>(1);
+	const setTitle = useSetRecoilState(titleState);
+	const setWineList = useSetRecoilState(wineListState);
+	const ListPageRendering = async () => {
+		try {
+			const data = await getWineList("france", page);
+			setTitle({ country: data.breadcrumbs[1].name, wine: data.breadcrumbs[0].slug });
+			setWineList(data.items);
+		} catch {
+			alert("server error");
+			new Error("server error");
+		}
+	};
+	useEffect(() => {
+		ListPageRendering();
+	}, [page]);
 	return (
 		<>
 			<Category />
 			<ListTitle />
 			<WineList />
+			<Pagination page={page} setPage={setPage} />
 		</>
 	);
 };
