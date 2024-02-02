e2565b9ab79266eff1cafbb45035a74d0b356ae3
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-03 00:03:49 +0900

    feat : persist page

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -1,6 +1,11 @@
 import { atom } from "recoil";
 import { WineType, titleType } from "../types";
+import { recoilPersist } from "recoil-persist";
 
+const { persistAtom } = recoilPersist({
+  key: "pageState",
+  storage: sessionStorage,
+});
 //title
 export const titleState = atom<titleType>({
   key: "titleState",
@@ -17,4 +22,18 @@ export const wineListState = atom<WineType[]>({
 export const loadingState = atom<boolean>({
   key: "loadingState",
   default: true,
+});
+
+//pageName
+export const pageNameState = atom<string>({
+  key: "pageNameState",
+  default: "list",
+  effects: [persistAtom],
+});
+
+//page
+export const pageState = atom<number>({
+  key: "pageState",
+  default: 1,
+  effects: [persistAtom],
 });
