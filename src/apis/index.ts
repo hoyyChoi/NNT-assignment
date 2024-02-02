5da82b32af26e75f6a3ac6c023041243f5547db0
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-02 19:34:10 +0900

    chore : delete annotation

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,24 +1,24 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export const WINEAPI = axios.create({
-	baseURL: "https://api.veenoverse.com/ssr/v2",
-	headers: {
-		"Content-Type": "application/json;charset=UTF-8",
-	},
-	responseType: "json",
+  baseURL: "https://api.veenoverse.com/ssr/v2",
+  headers: {
+    "Content-Type": "application/json;charset=UTF-8",
+  },
+  responseType: "json",
 });
 
 interface ApiResponse<T = any> {
-	data: T;
+  data: T;
 }
 
-//TODO: GET 메서드
+//GET 메서드
 export const getData = async <T>(url: string, config?: AxiosRequestConfig): Promise<ApiResponse<T>> => {
-	try {
-		const response = await WINEAPI.get(url, config);
-		return response.data;
-	} catch (error) {
-		console.log(error);
-		throw new Error();
-	}
+  try {
+    const response = await WINEAPI.get(url, config);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error();
+  }
 };
