7595e8cfffed0aa3d8b0917283ee3d14e18ac063
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-03 00:03:38 +0900

    feat : scrollToTop component

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ListPage } from "../pages/ListPage";
 import { DetailPage } from "../pages/DetailPage";
 import { Layout } from "../layout/Layout";
+import { ScrollToTop } from "../pages/common/ScrollToTop";
 
 export const Router = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<ListPage />} />
