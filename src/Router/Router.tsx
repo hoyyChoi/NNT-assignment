7b3624a8f46707f5a6e173ab756566e56d19b9ad
hoyyChoi <110888511+hoyyChoi@users.noreply.github.com>
2024-02-02 19:36:19 +0900

    fix : detailPage

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ListPage } from "../pages/ListPage";
 import Layout from "../Layout";
-import DetailPage from "../pages/DetailPage";
+import { DetailPage } from "../pages/DetailPage";
 
 export const Router = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route element={<Layout />}>
-					<Route path="/" element={<ListPage />} />
-					<Route path="/detail/:slug" element={<DetailPage />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<ListPage />} />
+          <Route path="/detail/:slug" element={<DetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,5 +1,13 @@
-const DetailPage = () => {
-	return <div>DetailPage</div>;
+import styled from "styled-components";
+
+export const DetailPage = () => {
+  return <Container>NOT FOUND</Container>;
 };
 
-export default DetailPage;
+const Container = styled.div`
+  font-size: 30px;
+  font-weight: 600;
+  color: red;
+  text-align: center;
+  margin-top: 150px;
+`;
